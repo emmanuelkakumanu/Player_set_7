b8de433538d30f9db768a60f4fceb469890fcd6c
emmanuelkakumanu <41730018+emmanuelkakumanu@users.noreply.github.com>
2018-08-24 17:49:58 +0530

    elements less than K in sorted order

---
+++ 64.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		int k=s.nextInt();
+		int a[]=new int[n];
+		for(int i=0;i<n;i++)
+		a[i]=s.nextInt();
+		
+		for(int i=0;i<n;i++)
+		{
+			for(int j=i+1;j<n;j++)
+			{
+				if(a[i]>a[j])
+				{
+					int t=a[i];
+					a[i]=a[j];
+					a[j]=t;
+				}
+			}
+		}
+		if(a[0]<k)
+		System.out.print(a[0]);
+		for(int i=1;a[i]<k;i++)
+		System.out.print(" "+a[i]);
+	}
+}
