8f129da1c9bb69e6bdcd9a4334f6ea39b8ec1d6b
emmanuelkakumanu <41730018+emmanuelkakumanu@users.noreply.github.com>
2018-08-25 10:52:35 +0530

    length of longest repeating sequence

---
+++ 68.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		int a[]=new int[n];
+		for(int i=0;i<n;i++)
+		a[i]=s.nextInt();
+		int mc=0;
+		for(int i=0;i<n;i++)
+		{
+			int cnt=0;
+			for(int j=0;j<n;j++)
+			{
+				if(a[i]==a[j])
+				cnt++;
+				
+			}
+			if(cnt>mc)
+			{
+				mc=cnt;
+				
+			}
+		}
+		System.out.print(mc);
+	}
+}
